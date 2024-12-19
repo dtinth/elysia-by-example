56025402f3d6ab4d870e2a4b138b5666dc3dbdca
dtinth on MBP M1 <dtinth@spacet.me>
2024-12-19 18:32:30 +0700

    ‌

--- README.md
+++ README.md
@@ -5,6 +5,18 @@ In Bun, you can start a web server without having to explicitly call
 the `listen` method — just `export default` an object that has a
 `fetch` method.
 
+When you run:
+
+~~~sh
+bun examples/00_wintercg/index.ts
+~~~
+
+You should see:
+
+~~~
+Started development server: http://localhost:3000
+~~~
+
 </td><td width="400" valign="top">
 
 ```ts
--- examples/00_wintercg/example.ts
+++ examples/00_wintercg/example.ts
@@ -4,6 +4,18 @@ export default markdown`
 In Bun, you can start a web server without having to explicitly call
 the \`listen\` method — just \`export default\` an object that has a
 \`fetch\` method.
+
+When you run:
+
+~~~sh
+bun examples/00_wintercg/index.ts
+~~~
+
+You should see:
+
+~~~
+Started development server: http://localhost:3000
+~~~
 `;
 
 export const module = await import("./index");
